3795255e13d5f7c1bd5fe10d37abb33e06902a78
lexi <byueyue022@gmail.com>
2022-01-20 15:24:38 -0600

    state hook

--- co-typing/src/App.js
+++ co-typing/src/App.js
@@ -1,36 +1,46 @@
+import { click } from '@testing-library/user-event/dist/click'
 import React, {useState} from 'react'
 
-const App = () =>{
-  const [counter, setCounter] = useState(0)
-
-  const Plus =() =>
-    setCounter(counter+1)
-  
-
-  const Zero = () =>
-    setCounter(0)
-  
-  const Display = ({counter}) =>{
+const History = ({allClicks}) =>{
+  if(allClicks.length === 0){
     return(
       <div>
-        {counter}
-      </div>)
-  }
-
-  const Button =({name,text}) =>{
-
-    return(
-      <button onClick = {name}>
-        {text}
-        </button>
+        This app is used by clicking the buttons
+      </div>
     )
   }
-  
-  return (
+  return(
     <div>
-      <Display counter = {counter}/>
-      <Button name = {Plus} text = 'Plus' />
-      <Button name = {Zero} text = 'Plus' />
+      button clicking history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({name,text}) =>{
+  return(
+  <button onClick={name}>{text}</button>
+)}
+
+const App = () =>{
+  const[left,setLeft] = useState(0)
+  const[right,setRight] = useState(0)
+  const[allClicks,setAll] = useState([])
+  const handleleftClick = () =>{
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+  }
+
+  const handlerightClick = () =>{
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+  }
+  return(
+    <div>
+      {left}
+      <Button name={handleleftClick} text = 'left' />
+      <Button name={handlerightClick} text = 'right' />
+      {right}
+      <History allClicks={allClicks}/>
     </div>
   )
 }
